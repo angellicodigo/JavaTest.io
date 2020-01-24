49cf082b384f087a94305453be99ac8e2980e7a8
angellicodigo <55287627+angellicodigo@users.noreply.github.com>
2020-01-24 11:28:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,11 +22,23 @@ for(var y=0; y<=5;y++){
  }
 
 function myLoop(){
-  var end =  document.getElementById("end").value;
+  var end = document.getElementById("end").value;
     var start = document.getElementById("start").value;
+    var jump = document.getElementById("jump").value;
 
-
-  for(var i=start;i<=end;i++){
+console.log(typeof start);
+ console.log(typeof end);
+  console.log(typeof jump);
+  
+  start = Number(start);
+  end = Number(end);
+  jump = Number(jump);
+  
+  for(var i=start;i<=end;i+=jump){
+    document.write('<p>'+i);
+    
+  }
+    for(var i=start;i>=end;i-=jump){
     document.write('<p>'+i);
   }
 }
